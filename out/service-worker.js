070ee91d9cab72687313964032898f7f2f26af30
Harun Memiş <harun.memis@fabrikamedya.com.tr>
2019-05-15 15:35:34 +0300

    Deploy to gh-pages

--- out/service-worker.js
+++ out/service-worker.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = [
   {
-    "url": "/_next/static/WwwwZKKuNSjtREb8fUYUF/pages/_app.js",
-    "revision": "15b5055ce2985a2ceb59"
+    "url": "/_next/static/67ShK2s4r7pHLhQ9A6azE/pages/_app.js",
+    "revision": "ad0a456184b67324e4c8"
   },
   {
-    "url": "/_next/static/WwwwZKKuNSjtREb8fUYUF/pages/_error.js",
-    "revision": "7b7de18bb35107985162"
+    "url": "/_next/static/67ShK2s4r7pHLhQ9A6azE/pages/_error.js",
+    "revision": "559d21c34f0a25a7ac8e"
   },
   {
-    "url": "/_next/static/WwwwZKKuNSjtREb8fUYUF/pages/index.js",
-    "revision": "c2c0ecdf593034ea2e52"
+    "url": "/_next/static/67ShK2s4r7pHLhQ9A6azE/pages/index.js",
+    "revision": "f423b4fc7f8a6aa80c24"
   },
   {
     "url": "/_next/static/chunks/commons.86fe20cb0bfa8987a73e.js",
