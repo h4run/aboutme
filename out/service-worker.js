0391b417009d4a5466cb90a15df34eac59c8c1aa
Harun Memiş <harun.memis@fabrikamedya.com.tr>
2019-05-15 16:02:10 +0300

    Deploy to gh-pages

--- out/service-worker.js
+++ out/service-worker.js
@@ -12,24 +12,24 @@ self.__precacheManifest = [
     "revision": "9d27ef16ab784a967782"
   },
   {
-    "url": "/_next/static/ieBR09HBrg3K2x31BmtRO/pages/_app.js",
-    "revision": "8f45ab64d1066dc44f23"
-  },
-  {
-    "url": "/_next/static/ieBR09HBrg3K2x31BmtRO/pages/_error.js",
-    "revision": "703e7aec2211b70eaded"
-  },
-  {
-    "url": "/_next/static/ieBR09HBrg3K2x31BmtRO/pages/index.js",
-    "revision": "b51e15a1809f3ed487ee"
-  },
-  {
     "url": "/_next/static/runtime/main-fd508ba2eaf92d54803d.js",
     "revision": "0ceef72b97a301802689"
   },
   {
     "url": "/_next/static/runtime/webpack-8ed9452df514b4d17d80.js",
     "revision": "c9d4f4ac318bec9c37e2"
+  },
+  {
+    "url": "/_next/static/tkO19YK7Gqz2zK2n96UfY/pages/_app.js",
+    "revision": "530bd5f0ffb739b0b5cb"
+  },
+  {
+    "url": "/_next/static/tkO19YK7Gqz2zK2n96UfY/pages/_error.js",
+    "revision": "d39b2f446d1b3b97d96b"
+  },
+  {
+    "url": "/_next/static/tkO19YK7Gqz2zK2n96UfY/pages/index.js",
+    "revision": "58b57e8ed7f2af2dc1bd"
   }
 ];
 
