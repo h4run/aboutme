ad0a6a2546923e3adfcb63b3cc73facf3d0f080d
Harun <harun.memis@fabrikamedya.com.tr>
2019-05-22 13:17:54 +0300

    offline

--- next.config.js
+++ next.config.js
@@ -3,49 +3,19 @@ const offline = require("next-offline");
 
 const withPlugins = require("next-compose-plugins");
 
-module.exports = withPlugins(
-  [
-    css,
-    [
-      offline,
-      {
-        workboxOpts: {
-          swDest: "static/service-worker.js",
-          runtimeCaching: [
-            {
-              urlPattern: /^https?.*/,
-              handler: "NetworkFirst",
-              options: {
-                cacheName: "https-calls",
-                networkTimeoutSeconds: 15,
-                expiration: {
-                  maxEntries: 150,
-                  maxAgeSeconds: 30 * 24 * 60 * 60
-                },
-                cacheableResponse: {
-                  statuses: [0, 200]
-                }
-              }
-            }
-          ]
-        }
+module.exports = withPlugins([css, offline], {
+  webpack(config) {
+    config.module.rules = config.module.rules.map(rule => {
+      if (rule.loader === "babel-loader") {
+        rule.options.cacheDirectory = false;
       }
-    ]
-  ],
-  {
-    webpack(config) {
-      config.module.rules = config.module.rules.map(rule => {
-        if (rule.loader === "babel-loader") {
-          rule.options.cacheDirectory = false;
-        }
-        return rule;
-      });
+      return rule;
+    });
 
-      config.module.rules.push({
-        test: /\.md$/,
-        use: "raw-loader"
-      });
-      return config;
-    }
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader"
+    });
+    return config;
   }
-);
+});
